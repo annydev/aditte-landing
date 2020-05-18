62ff0201f72c1fa1e1dc3cce7aa1dd1445d9eb00
annydev <dandara.anna13@hotmail.com>
2020-05-19 00:20:36 +0300

    mobile design part-1

--- js/index.js
+++ js/index.js
@@ -15,25 +15,39 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
 
-window.addEventListener('load', function(){
+window.addEventListener('load', function() {
   new Glider(document.querySelector('.glider'), {
-    slidesToShow:3 ,
-  slidesToScroll: 3,
+    slidesToShow: 1,
+    slidesToScroll: 1,
     arrows: {
       prev: '.prev',
       next: '.next'
+    },
+    responsive: [
+      {
+      // screens greater than >= 1024px
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3
+      }
     }
+  ]
   })
 })
